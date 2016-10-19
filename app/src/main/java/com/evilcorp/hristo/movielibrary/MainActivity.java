da78033dc521b45fa5afee7369a91ca672ce7334
TIRBU6ONA <hristo.j.s@gmail.com>
2016-10-20 01:56:38 +0300

    Added VolleyRequest class

--- app/src/main/java/com/evilcorp/hristo/movielibrary/MainActivity.java
+++ app/src/main/java/com/evilcorp/hristo/movielibrary/MainActivity.java
@@ -3,11 +3,15 @@ package com.evilcorp.hristo.movielibrary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
