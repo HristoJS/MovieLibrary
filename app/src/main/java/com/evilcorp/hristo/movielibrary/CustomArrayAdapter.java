e0d9419cbcc1aa940578d77c45c829a87df703cf
TIRBU6ONA <hristo.j.s@gmail.com>
2016-10-22 01:44:41 +0300

    Added data to listview

--- app/src/main/java/com/evilcorp/hristo/movielibrary/CustomArrayAdapter.java
+++ app/src/main/java/com/evilcorp/hristo/movielibrary/CustomArrayAdapter.java
@@ -1,14 +1,20 @@
 package com.evilcorp.hristo.movielibrary;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,22 +34,32 @@ public class CustomArrayAdapter extends ArrayAdapter<Movie> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        boolean completed = movies.get(position).Response;
-        Log.d("Test",completed+"");
-        //String item = getItem(position);
-        // The child views in each row.
-        CheckBox checkBox ;
-        TextView textView ;
+        Movie movie = getItem(position);
 
-        convertView = inflater.inflate(R.layout.list_item, null);
+        // Check if an existing view is being reused, otherwise inflate the view
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
 
         // Find the child views.
-//        textView = (TextView) convertView.findViewById( R.id.step_view );
-//        checkBox = (CheckBox) convertView.findViewById( R.id.check_box );
-//        checkBox.setChecked(true);
-//        Log.d("Checked",checkBox.isChecked()+"");
-//
-//        textView.setText(item);
+        TextView titleText = (TextView) convertView.findViewById( R.id.titleText );
+        TextView yearText = (TextView) convertView.findViewById( R.id.yearText );
+        TextView genreText = (TextView) convertView.findViewById( R.id.genreText );
+        ImageView moviePoster = (ImageView) convertView.findViewById( R.id.moviePoster );
+
+        // Set the value of each cell
+        titleText.setText(movie.Title);
+        yearText.setText(movie.Year);
+        genreText.setText(movie.Genre);
+
+        // For the Image , convert to bitmap
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream((InputStream)new URL(movie.Poster).getContent());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        moviePoster.setImageBitmap(bitmap);
 
         return convertView;
     }
--- app/src/main/java/com/evilcorp/hristo/movielibrary/MainActivity.java
+++ app/src/main/java/com/evilcorp/hristo/movielibrary/MainActivity.java
@@ -1,14 +1,12 @@
 package com.evilcorp.hristo.movielibrary;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.CheckedTextView;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Spinner;
@@ -20,7 +18,6 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
